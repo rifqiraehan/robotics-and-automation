2a515c8627db46a7895c94c72f171f60f4422ea7
rifqiraehan <rifqiraehan86@gmail.com>
2025-09-04 22:57:40 +0700

    add new project to handle trackbar of position

---
+++ Sudut/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using CommunityToolkit.Mvvm.DependencyInjection; // Jika gunakan IoC, optional
+
+namespace Sudut
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // DataContext sudah di-set di XAML; jika perlu IoC: DataContext = Ioc.Default.GetService<ServoViewModel>();
+        }
+    }
+}
