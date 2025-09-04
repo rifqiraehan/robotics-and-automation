73962c8047f6f043f122a26da7bd91c06f137f79
rifqiraehan <rifqiraehan86@gmail.com>
2025-09-04 22:56:34 +0700

    obj debug changed after run

--- SerialComm/obj/Debug/net8.0-windows/SerialComm.AssemblyInfo.cs
+++ SerialComm/obj/Debug/net8.0-windows/SerialComm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SerialComm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85b696e2543a4d082ff0d3e2f8ef9b4cd831b7bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SerialComm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SerialComm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
